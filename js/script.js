8407b27c911b4b0134dc6d82ad3184c2280f9082
Yonathan-Roblero-Soto <yonathan.roblero17@tectijuana.edu.mx>
2020-06-05 17:20:04 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+  });
